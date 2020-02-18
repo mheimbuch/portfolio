bc0c345b05e8334fbc336b33f1be05fbfaf9baee
Alexander Heimbuch <github@heimbu.ch>
2020-02-18 18:27:07 +0100

    chore: fix social links

--- src/components/Social.js
+++ src/components/Social.js
@@ -10,7 +10,7 @@ export default class Social extends React.Component {
                 <h2>{_.get(this.props, 'pageContext.site.data.social.title')}</h2>
                 <ul className="icons">
                     {_.map(_.get(this.props, 'pageContext.site.data.social.follow_section'), (link, link_idx) => (
-                        <li key={link_idx}><Link to={_.get(link, 'link_url')} className={'icon style2 ' + _.get(link, 'icon')}><span className="label">{_.get(link, 'title')}</span></Link></li>
+                        <li key={link_idx}><a href={_.get(link, 'link_url')} className={'icon style2 ' + _.get(link, 'icon')}><span className="label">{_.get(link, 'title')}</span></a></li>
                     ))}
                 </ul>
             </section>
